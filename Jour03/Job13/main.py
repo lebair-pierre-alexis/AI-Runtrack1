b78a590798e18647263a362d03bc27dda0a6b33a
lebair-pierre-alexis <pierre-alexis.lebair@laplateforme.io>
2022-09-26 12:59:13 +0200

    Day03/Job13 added

--- Jour03/Job13/main.py
+++ Jour03/Job13/main.py
@@ -1,5 +1,25 @@
+from collections import OrderedDict
 import re
 import matplotlib.pyplot as plt
 
 regex = "(([a-z]|[A-Z])+)"
 data = {}
+
+f = open("data.txt", "rt")
+text = f.read()
+result = re.findall(regex, text)
+for i in range(len(result)):
+    letter = result[i][0][0].lower()
+    if data.setdefault(letter) == None:
+        data[letter] = 1
+    else:
+        data[letter] += 1
+
+data = OrderedDict(sorted(data.items()))
+
+plt.bar(list(data.keys()), list(data.values()))
+plt.title("Nombre d'occurence de chaque lettre en début de mot")
+plt.xlabel("Lettres")
+plt.ylabel("Nombre d'occurence")
+plt.show()
+f.close()
